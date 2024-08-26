23e7a3bb2c595969c660fc66abaa66a905ab2083
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2024-08-26 13:33:11 -0300

    Realice todas las pruebas

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,8 @@
 from classes.jugador import Jugador
+from classes.apuesta import Apuesta
 
-
+#Jugador ----------------------------------------
+"""
 j = Jugador("Juan")
 j2 = Jugador("maria", 10)
 
@@ -14,3 +16,21 @@ print(j)
 
 esVerda = j.sinFichas()
 print(esVerda)
+"""
+
+#Apuestas----------------------------------------
+
+a = Apuesta()
+
+a.ponerFicha(76)
+a.tomarFicha(76)
+
+a.ponerFicha(75)
+print(a)
+
+a.tomarTodas()
+print(a)
+
+a.ponerFicha(6)
+resultado = a.estaVacia()
+print(resultado)
